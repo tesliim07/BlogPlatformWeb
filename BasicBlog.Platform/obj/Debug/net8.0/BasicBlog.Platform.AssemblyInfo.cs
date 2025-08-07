4792ad61562f8ceb8b26ffa3c928ea6824c7ed7d
tesliim07 <up2079143@myport.ac.uk>
2025-08-07 22:45:11 +0100

    deleted docker-come file

--- BasicBlog.Platform/obj/Debug/net8.0/BasicBlog.Platform.AssemblyInfo.cs
+++ BasicBlog.Platform/obj/Debug/net8.0/BasicBlog.Platform.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicBlog.Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29b41559a35e7cd6c68ee2578bc455072761d20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a190882d66ca74cdf2a2b6b1ce920c5b526d2c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicBlog.Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicBlog.Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
